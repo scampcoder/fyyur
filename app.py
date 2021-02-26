333cf8f60e2fa1add2cc225bea904cb07c6cb503
Victoria Kee <victoriakee95@gmail.com>
2021-02-26 00:07:40 -0600

    Add show model

--- app.py
+++ app.py
@@ -57,7 +57,14 @@ class Artist(db.Model):
     facebook_link = db.Column(db.String(120))
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
+class Show(db.Model):
+  __tablename__ = 'Show'
 
+  id = db.Column(db.Integer, primary_key=True)
+  start_time = db.Column(db.DateTime)
+  venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
+  artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
+  artist_image_link = db.Column(db.String(), db.ForeignKey('Artist.image_link'))
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
 
 #----------------------------------------------------------------------------#
