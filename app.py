c879d139579de28fed832f1f3f07dd75ce87ae9b
Victoria Kee <victoriakee95@gmail.com>
2021-02-26 00:13:04 -0600

    Update venues pg

--- app.py
+++ app.py
@@ -110,28 +110,14 @@ def index():
 def venues():
   # TODO: replace with real venues data.
   #       num_shows should be aggregated based on number of upcoming shows per venue.
-  data=[{
-    "city": "San Francisco",
-    "state": "CA",
-    "venues": [{
-      "id": 1,
-      "name": "The Musical Hop",
-      "num_upcoming_shows": 0,
-    }, {
-      "id": 3,
-      "name": "Park Square Live Music & Coffee",
-      "num_upcoming_shows": 1,
-    }]
-  }, {
-    "city": "New York",
-    "state": "NY",
-    "venues": [{
-      "id": 2,
-      "name": "The Dueling Pianos Bar",
-      "num_upcoming_shows": 0,
-    }]
-  }]
-  return render_template('pages/venues.html', areas=data);
+  @app.route('/venues')
+def venues():
+  # TODO: replace with real venues data.
+  #       num_shows should be aggregated based on number of upcoming shows per venue.
+  venues=Venue.query.all()
+  places = Venue.query.distinct(Venue.city, Venue.state).all()
+  return render_template('pages/venues.html', venues=venues, places=places)
+
 
 @app.route('/venues/search', methods=['POST'])
 def search_venues():
